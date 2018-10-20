d7ab204ce3d846bf4e92060de1ecd581e557e83d
SilverAce13 <deryketang@gmail.com>
2018-10-19 23:31:42 -0400

    Update brhbot.js

--- brhbot.js
+++ brhbot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+/*const client = new Discord.Client();
  
 client.on("ready", () => {
   console.log("I am ready!");
 });
-/* 
+
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
@@ -15,7 +15,7 @@ client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.DqwnXw.RpLqKTRMkqObOegOz5aGWD7o6rg");
 */
 var auth = require('./auth.json');
 
-var bot = new Discord.Client({
+const bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
@@ -23,6 +23,7 @@ var bot = new Discord.Client({
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+	console.log("I am ready!");
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
