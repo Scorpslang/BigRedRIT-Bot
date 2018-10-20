a6f19031a18809ee88c5afd4886d50133399c1c7
SilverAce13 <deryketang@gmail.com>
2018-10-19 23:36:34 -0400

    Update brhbot.js

--- brhbot.js
+++ brhbot.js
@@ -1,31 +1,13 @@
 const Discord = require("discord.js");
-/*const client = new Discord.Client();
+const client = new Discord.Client();
  
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
- 
-client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.DqwnXw.RpLqKTRMkqObOegOz5aGWD7o6rg");
-*/
-var auth = require('./auth.json');
-
-const bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-	console.log("I am ready!");
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+	if(message.substring(0,1) == "!"){
+		var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
@@ -44,7 +26,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					message: 'Hello'
 				})
 			break;
-            // Just add any case commands if you want to..
-         }
-     }
+		}
+	}
 });
+ 
+client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.DqwnXw.RpLqKTRMkqObOegOz5aGWD7o6rg");
