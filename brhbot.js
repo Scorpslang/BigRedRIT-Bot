b3f4ea9075b4372b91e9d363e0b048ffd7330215
Scorpslang <36864800+Scorpslang@users.noreply.github.com>
2018-10-19 22:56:11 -0400

    revert

--- brhbot.js
+++ brhbot.js
@@ -1,36 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var auth = require('./auth.json');
-
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
 });
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			// !greet
-			case 'greet':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Hello'
-				})
-			break;
-            // Just add any case commands if you want to..
-         }
-     }
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
 });
+ 
+client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.DqwnXw.RpLqKTRMkqObOegOz5aGWD7o6rg");
