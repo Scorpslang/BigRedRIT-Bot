96cba9a39b6829acb8c4ae569043540fa468aab7
Scorpslang <36864800+Scorpslang@users.noreply.github.com>
2018-10-20 00:59:54 -0400

    cccchanges

--- brhbot.js
+++ brhbot.js
@@ -4,13 +4,17 @@ const config = require("./config.json");
  
 client.on("ready", () => {
   console.log("I am ready!");
+  client.channels.get('503044022781083648').send("And I'm back");
 });
  
 client.on("message", (message) => {
 if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  if (message.content.startsWith("ping")) {
+  if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
   }
+  if (message.content.startsWith(config.prefix + "greet")) {
+    message.channel.send("Hello World");
+  }
 });
- 
+
 client.login(config.token);
