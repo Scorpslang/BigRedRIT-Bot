640df10a18583383f2cb2d22b547e0f5bbc11e08
SilverAce13 <deryketang@gmail.com>
2018-10-19 20:44:17 -0400

    Update brhbot.js

--- brhbot.js
+++ brhbot.js
@@ -1,14 +1,46 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
  
-client.on("ready", () => {
-  console.log("I am ready!");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
 });
- 
-client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.Dqv-DA.dAtsjbWNe2dtqmR0s665OuwKy7Y");
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+			// !greet
+			case 'greet':
+				bot.sendMessage({
+					to: channelID,
+					message: 'Hello'
+				})
+			break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
