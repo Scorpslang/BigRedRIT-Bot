7883b3fa3b13e8a4e3beb0c923619e4f42ce5623
Scorpslang <36864800+Scorpslang@users.noreply.github.com>
2018-10-19 23:52:45 -0400

    added config

--- brhbot.js
+++ brhbot.js
@@ -1,41 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const config = require("./config.json");
  
 client.on("ready", () => {
   console.log("I am ready!");
 });
-
+ 
 client.on("message", (message) => {
-<<<<<<< HEAD
+if (!message.content.startsWith(config.prefix) || message.author.bot) return;
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }
-  if (message.author.bot) return;
-=======
-	if(message.substring(0,1) == "!"){
-		var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			// !greet
-			case 'greet':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Hello'
-				})
-			break;
-		}
-	}
->>>>>>> a6f19031a18809ee88c5afd4886d50133399c1c7
 });
  
-client.login("NTAyOTg5MDUxNDAyNjQ5NjE3.DqwnXw.RpLqKTRMkqObOegOz5aGWD7o6rg");
+client.login(config.token);
